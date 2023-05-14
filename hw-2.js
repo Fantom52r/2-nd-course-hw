d67f071798ae785ab63f819939d157f4a1f462ff
Fantom52r <fantom52rs@gmail.com>
2023-05-14 16:38:31 +0300

    zadanie 9

---
+++ hw-2.js
@@ -0,0 +1,44 @@
+// Задание 1
+// let a = 20
+// alert(a)
+
+// Задание 2
+// let year = 2007
+// alert(a)
+
+// Задание 3
+// let creator = "Brendan Eich";
+// alert(creator)
+
+// Задание 4
+// let a = 10
+// let b = 2
+// alert(a + b)
+// alert(a - b)
+// alert(a * b)
+// alert(a / b)
+
+// Задание 5
+// let result = 2 ** 5
+// alert(result)
+
+// Задание 6
+// let a = 9
+// let b = 2
+// alert(a % b)
+
+// Задание 7
+// let num = 1;
+// num += 5;
+// num -= 3;
+// num *= 7;
+// num /= 3;
+// num += 1;
+// num -= 1;
+// alert(num);
+
+// Задание 8
+// let age = prompt ("Сколько вам лет?")
+// alert(age)
+
+// Задание 9
