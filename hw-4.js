e0bed0ef9fac3e9442bd92306bf6f4330fa747e0
Fantom52r <fantom52rs@gmail.com>
2023-05-19 23:42:24 +0300

    example 3

---
+++ hw-4.js
@@ -0,0 +1,36 @@
+// Задание 1
+
+// let i = 0;
+// while (i < 2) {
+//     console.log('Привет');
+//     i++
+// }
+
+// Задание 2 
+
+// let i = 1
+// while (i < 6 ) {
+//     console.log(i);
+//     i++
+// }
+
+// Задание 3
+
+// let i = 7
+// while (i < 23) {
+//     console.log(i);
+//     i++
+
+// }
+
+// Задание 4
+
+const obj = {
+    Коля: 200,
+    Вася: 300,
+    Петя: 400,
+};
+for (let salary in obj) {
+        
+    alert([salary] obj[salary]);
+}
