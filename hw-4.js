16d84fbd00bd8353f2e05fd287ea35eed8d39967
Fantom52r <fantom52rs@gmail.com>
2023-05-21 18:35:33 +0300

    added new file hw-4.js

--- hw-4.js
+++ hw-4.js
@@ -25,12 +25,27 @@
 
 // Задание 4
 
-const obj = {
-    Коля: 200,
-    Вася: 300,
-    Петя: 400,
-};
-for (let salary in obj) {
-        
-    alert([salary] obj[salary]);
-}
+// const obj = {
+//     Коля: 200,
+//     Вася: 300,
+//     Петя: 400,
+// };
+// for (let name in obj ) {
+// console.log(`${name} - Зарплата ${obj[name]} Долларов`);
+// }
+
+// Задание 5
+
+// let n = 1000;
+// let num = 0;
+// while (n > 50) {
+//     n /= 2;
+//     num++;
+// }console.log(n);
+
+// Задание 6
+
+// for (let dayNumber = 5; dayNumber <= 31; dayNumber += 7) {
+
+//     console.log(`Сегодня пятница,${dayNumber}-е число. Необходимо подготовить отчет.`);
+// }
