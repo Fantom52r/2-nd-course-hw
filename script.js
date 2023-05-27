0c4a48df763dae4ae69bc340d552a511174a22fe
Fantom52r <fantom52rs@gmail.com>
2023-05-28 01:14:25 +0300

    changed fila - final 2.5

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+// function seasons(Number) {
+
+//      Number = prompt('Введите число');
+
+//     if (Number == 12 || Number <= 2)  {
+//         alert('Зима')
+//     } else if (Number == 3 || Number <= 5) {
+//         alert('Весна')
+//     } else if (Number == 6 || Number <= 8) {
+//         alert('Лето')
+//     } else if (Number == 9 || Number <= 11 ) {
+//         alert('Осень')
+//     } else {
+//         alert('такого времени не существует , ты проиграл')
+//     } 
+// }
+// seasons(userInput)
