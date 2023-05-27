43f6f72127e1c0506cd998d05399dc66da146b8d
Fantom52r <fantom52rs@gmail.com>
2023-05-28 01:20:49 +0300

    chanded file

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
 
 
-// function seasons(Number) {
+function seasons(Number) {
 
-//      Number = prompt('Введите число');
+    let Number = prompt('Введите число');
 
-//     if (Number == 12 || Number <= 2)  {
-//         alert('Зима')
-//     } else if (Number == 3 || Number <= 5) {
-//         alert('Весна')
-//     } else if (Number == 6 || Number <= 8) {
-//         alert('Лето')
-//     } else if (Number == 9 || Number <= 11 ) {
-//         alert('Осень')
-//     } else {
-//         alert('такого времени не существует , ты проиграл')
-//     } 
-// }
-// seasons(userInput)
+    if (Number == 12 || Number <= 2) {
+        alert('Зима')
+    } else if (Number == 3 || Number <= 5) {
+        alert('Весна')
+    } else if (Number == 6 || Number <= 8) {
+        alert('Лето')
+    } else if (Number == 9 || Number <= 11) {
+        alert('Осень')
+    } else {
+        alert('такого времени не существует , ты проиграл')
+    }
+}
